6181f9496a2179ce5a70933d235c352bf28ae580
John Elesho <eleshojohno@gmail.com>
2020-10-31 23:39:50 +0100

    Designed the students and courses model

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET']= 'elesho'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todoapi.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///studcourse.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
@@ -16,16 +16,16 @@ class Student(db.Model):
     reg_no = db.Column(db.String(11), unique=True, nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    courses = db.Column(db.String(100), nullable=True)
+    is_couse_rep = db.Column(db.Boolean, default=False, nullable=False)
 
-class Student(db.Model):
+class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reg_no = db.Column(db.String(11), unique=True, nullable=False)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    course_code = db.Column(db.String(5), unique=True, nullable=False)
+    course_title = db.Column(db.String(50), unique=True, nullable=False)
+    course_unit = db.Column(db.Integer, nullable=False, default=1)
 
-
+# 
 
 
 if __name__ == "__main__":
